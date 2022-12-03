e1e27be27e83464a27a620641cd9d44b2f9d3d1b
Zicos <1179382172@qq.com>
2022-12-03 14:25:32 +0800

    :sparkles:完成跨域问题以及测试前后联调

--- system-admin/src/main/java/com/lzc/controller/TestController.java
+++ system-admin/src/main/java/com/lzc/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.lzc.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
